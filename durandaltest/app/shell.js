612faad4be1531eb59f992e1fd373f1c315841a3
petegerhat <pete.gerhat@gmail.com>
2015-11-23 14:09:38 +0000

    Tabs working

--- durandaltest/app/shell.js
+++ durandaltest/app/shell.js
@@ -3,7 +3,11 @@
         router: router,
         activate: function () {
             router.map([
-                { route: '', title:'1. Import', moduleId: 'app/home', nav: true },
+                { route: '', title:'1. Import', moduleId: 'app/tabs/import', nav: true },
+                { route: 'validate', title:'2. Validate', moduleId: 'app/tabs/validate', nav: true },
+                { route: 'organize', title:'3. Organize', moduleId: 'app/tabs/organize', nav: true },
+                { route: 'preview', title:'4. Preview', moduleId: 'app/tabs/preview', nav: true },
+                { route: 'export', title:'5. Export', moduleId: 'app/tabs/export', nav: true },
             ]).buildNavigationModel();
             
             return router.activate();
