83526927730b00330a990f5380b043d8ce1b8d6f
petegerhat <pete.gerhat@gmail.com>
2015-11-23 12:19:59 +0000

    Adjustments start

--- durandaltest/app/shell.js
+++ durandaltest/app/shell.js
@@ -1,11 +1,6 @@
 ﻿define(['plugins/router', 'durandal/app'], function (router, app) {
     return {
         router: router,
-        search: function() {
-            //It's really easy to show a message box.
-            //You can add custom options too. Also, it returns a promise for the user's response.
-            app.showMessage('Search not yet implemented...');
-        },
         activate: function () {
             router.map([
                 { route: '', title:'1. Import', moduleId: 'app/home', nav: true },
