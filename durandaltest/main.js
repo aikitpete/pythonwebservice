bdea43c8e0324f3fe05aef86e632557dd7652faa
petegerhat <pete.gerhat@gmail.com>
2015-11-23 15:05:41 +0000

    HTML copied

--- durandaltest/main.js
+++ durandaltest/main.js
@@ -20,15 +20,60 @@ requirejs.config({
         'plugins/http': 'lib/Durandal/js/plugins/http',
         'durandal/viewEngine': 'lib/Durandal/js/viewEngine',
         'transitions/entrance': 'lib/Durandal/js/transitions/entrance',
-        'bootstrap' :  '//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min'
+        'bootstrap' :  '//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min',
+        
+        'slick': '//cdn.jsdelivr.net/jquery.slick/1.5.7/slick.min',
+        'prettyprint': 'prettyprint.js?a=b',
+        'tabslideout': 'tabslideout.1.3.js?a=b',
+        'utils': '../js/utils',
+        
+        //'datatables': 'https://cdn.datatables.net/s/bs/dt-1.10.10,cr-1.3.0,rr-1.1.0,sc-1.4.0,se-1.1.0/datatables',
+        
+        //'datatables' : 'lib/DataTables/datatables',
+        //'datatables.net' : 'lib/DataTables/DataTables-1.10.10/js/jquery.dataTables',
+        'datatables.net-bs' : 'lib/DataTables/DataTables-1.10.10/js/dataTables.bootstrap',
+        
+        //STACKOVERFLOW
+        'datatables.net' : 'lib/DataTables/DataTables-1.10.10/js/jquery.dataTables',
+        'datatables' : 'lib/DataTables/DataTables-1.10.10/js/dataTables.bootstrap',
+        
+        
+        'datatables.net-autofill' : 'lib/DataTables/AutoFill-2.1.0/js/dataTables.autoFill',
+        'datatables.net-editor' : "lib/DataTables/Editor-1.5.2/js/dataTables.editor",
+        'datatables-editor-bootstrap' : "lib/DataTables/Editor-1.5.2/js/editor.bootstrap",
+        //'datatables.net-buttons' : 'lib/DataTables/Buttons-1.1.0/js/dataTables.buttons',
+        'datatables.net-buttons' : 'lib/DataTables/Buttons-1.1.0/js/buttons.bootstrap',
+        
+        //'datatables.net-buttons-bs' : 'lib/DataTables/Buttons-1.1.0/js/buttons.bootstrap',
+        'datatables.net-colreorder' : "lib/DataTables/ColReorder-1.3.0/js/dataTables.colReorder",
+        'datatables.net-rowreorder' : "lib/DataTables/RowReorder-1.1.0/js/dataTables.rowReorder",
+        'datatables.net-scroller' : "lib/DataTables/Scroller-1.4.0/js/dataTables.scroller",
+        'datatables.net-select' : "lib/DataTables/Select-1.1.0/js/dataTables.select",
     },
     shim: {
+        'jquery' : {
+            deps: [],
+            exports: '$'
+        },
         'bootstrap': {
             deps: ['jquery']
         },
         'durandal/app': {
             deps: ['bootstrap']
-        }
+        },
+        
+        'datatables' : {
+           deps: ['jquery','bootstrap','datatables.net-colreorder','datatables.net-rowreorder','datatables.net-scroller','datatables.net-select']  
+        },
+        'datatables.net-editor' : {
+            deps: ['jquery','bootstrap','datatables',],//,'datatables-editor-bootstrap','datatables.net-buttons','datatables.net-editor'
+        },
+        'tabslideout': {
+            deps: ['jquery','bootstrap']
+        },
+        'jquery-ui': {
+            deps: ['jquery']
+        },
     }
 });
 
